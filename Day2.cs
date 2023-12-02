6a193f9d98f79bc6a85fde62f667c37f29832010
ayoubdkhissi <1999dkhissi@gmail.com>
2023-12-02 16:20:28 +0000

    day2 - part 1

---
+++ Day2.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2023;
+public static class Day2
+{
+    public static int Part1(string[] input)
+    {
+        return input
+            .Select(line => new Game(line))
+            .Where(g => g.Possible)
+            .Sum(g => g.Id);
+    }
+}
+
+
+public class Game
+{
+    public int Id { get; set; }
+    public int MaxBlues { get; set;}
+    public int MaxReds { get; set; }
+    public int MaxGreens { get; set; }
+    public bool Possible { get; set; }
+    public Game(string line)
+    {
+        Id = int.Parse(line.Substring(5, line.IndexOf(':') - 5));
+        MaxBlues = GetMaxByColor('b', line);
+        MaxReds = GetMaxByColor('r', line);
+        MaxGreens = GetMaxByColor('g', line);
+        Possible = MaxReds <= MaxPossibleReds &&
+                   MaxGreens <= MaxPossibleGreens &&
+                   MaxBlues <= MaxPossibleBlues;
+    }
+    private int GetMaxByColor(char color, string line)
+    {
+        return line
+            .Select((c, index) =>
+            c == color ? int.Parse(line.Substring(index - 3, 2)) : -1)
+            .Where(item => item != -1)
+            .Max();
+    }
+
+    private static int MaxPossibleReds = 12;
+    private static int MaxPossibleGreens = 13;
+    private static int MaxPossibleBlues = 14;
+
+}
--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@
 
 Console.WriteLine($"Answer to day 1 part 1: {Day1.Part1(File.ReadAllLines("input-day1.txt"))}");
 Console.WriteLine($"Answer to day 1 part 2: {Day1.Part2(File.ReadAllLines("input-day1.txt"))}");
+Console.WriteLine($"Answer to day 2 part 1: {Day2.Part1(File.ReadAllLines("input-day2.txt"))}");
